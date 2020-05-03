4dbb36e745ede79f5bbfee7883c549448a3f860b
anubhav230 <anubhavsingh230@gmail.com>
2020-05-04 01:14:31 +0530

    flip coin singlet percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,10 +1,25 @@
 #!/bin/bash -x
+read -p "Enter how number for flipping the coin" number
 heads=1
 tails=0
-flipCoin=$((RANDOM%2))
-if [ $flipCoin -eq 1 ]
-then
-	echo "heads"
-else
-	echo "tails"
-fi
+headCount=0
+tailCount=0
+count=$number
+declare A dictionary
+while (($count>0))
+do
+	flipCoin=$(( RANDOM%2 ))
+
+	if [ $flipCoin -eq 1 ]
+	then
+		dictionary[$count]="heads"
+		((headCount++))
+	else
+		dictionary[$count]="tails"
+		((tailCount++))
+	fi
+((count--))
+done
+echo ${dictionary[@]}
+headPercent=$(echo "scale=2; ($headCount*100)/$number"|bc)
+tailPercent=$(echo "scale=2; ($tailCount*100)/$number"|bc)
