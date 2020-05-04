4b5d6020f2cd9e002f45c560f1d0543667ab8ae8
anubhav230 <anubhavsingh230@gmail.com>
2020-05-04 14:41:03 +0530

    triplet percentage calculated

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -4,41 +4,80 @@ heads=1
 tails=0
 heads2=2
 tails2=3
-hhCount=0
-htCount=0
-thCount=0
-ttCount=0
+heads3=4
+tails3=5
+hhhCount=0
+hhtCount=0
+hthCount=0
+httCount=0
+thhCount=0
+thtCount=0
+tthCount=0
+tttCount=0
 count=$number
 declare A dictionary
 while (($count>0))
 do
 	flipCoin1=$(( RANDOM%2 ))
 	flipCoin2=$(( RANDOM%2 + 2 ))
+	flipCoin3=$(( RANDOM%2 + 4 ))
 
-	if [[ $flipCoin1 -eq 1 && $flipCoin2 -eq 2 ]]
+	if [ $flipCoin1 -eq 1 ] && [ $flipCoin2 -eq 2 ] && [ $flipCoin3 -eq 4 ]
 	then
-		dictionary[$count]="HH"
-		((hhCount++))
+		dictionary[$count]="HHH"
+		((hhhCount++))
 	fi
-	 if [[ $flipCoin1 -eq 1 && $flipCoin2 -eq 3 ]]
+
+	if [ $flipCoin1 -eq 1 ] && [ $flipCoin2 -eq 2 ] && [ $flipCoin3 -eq 5 ]
         then
-                dictionary[$count]="HT"
-                ((htCount++))
+                dictionary[$count]="HHT"
+                ((hhtCount++))
         fi
-	 if [[ $flipCoin1 -eq 0 && $flipCoin2 -eq 3 ]]
+
+	if [ $flipCoin1 -eq 1 ] && [ $flipCoin2 -eq 3 ] && [ $flipCoin3 -eq 4 ]
         then
-                dictionary[$count]="TT"
-                ((ttCount++))
+                dictionary[$count]="HTH"
+                ((hthCount++))
         fi
-	 if [[ $flipCoin1 -eq 0 && $flipCoin2 -eq 2 ]]
+
+	if [ $flipCoin1 -eq 1 ] && [ $flipCoin2 -eq 3 ] && [ $flipCoin3 -eq 5 ]
         then
-                dictionary[$count]="TH"
-                ((thCount++))
+                dictionary[$count]="HTT"
+                ((httCount++))
         fi
+
+	if [ $flipCoin1 -eq 0 ] && [ $flipCoin2 -eq 2 ] && [ $flipCoin3 -eq 4 ]
+        then
+                dictionary[$count]="THH"
+                ((thhCount++))
+        fi
+
+	if [ $flipCoin1 -eq 0 ] && [ $flipCoin2 -eq 2 ] && [ $flipCoin3 -eq 5 ]
+        then
+                dictionary[$count]="THT"
+                ((thtCount++))
+        fi
+
+	if [ $flipCoin1 -eq 0 ] && [ $flipCoin2 -eq 3 ] && [ $flipCoin3 -eq 4 ]
+        then
+                dictionary[$count]="TTH"
+                ((tthCount++))
+        fi
+
+	if [ $flipCoin1 -eq 0 ] && [ $flipCoin2 -eq 3 ] && [ $flipCoin3 -eq 5 ]
+        then
+                dictionary[$count]="TTT"
+                ((tttCount++))
+        fi
+
 ((count--))
 done
 echo ${dictionary[@]}
-hhPercent=$(echo "scale=2; ($hhCount*100)/$number"|bc)
-htPercent=$(echo "scale=2; ($htCount*100)/$number"|bc)
-ttPercent=$(echo "scale=2; ($ttCount*100)/$number"|bc)
-thPercent=$(echo "scale=2; ($thCount*100)/$number"|bc)
+hhhPercent=$(echo "scale=2; ($hhhCount*100)/$number"|bc)
+hhtPercent=$(echo "scale=2; ($hhtCount*100)/$number"|bc)
+hthPercent=$(echo "scale=2; ($hthCount*100)/$number"|bc)
+httPercent=$(echo "scale=2; ($httCount*100)/$number"|bc)
+thhPercent=$(echo "scale=2; ($thhCount*100)/$number"|bc)
+thtPercent=$(echo "scale=2; ($thtCount*100)/$number"|bc)
+tthPercent=$(echo "scale=2; ($tthCount*100)/$number"|bc)
+tttPercent=$(echo "scale=2; ($tttCount*100)/$number"|bc)
