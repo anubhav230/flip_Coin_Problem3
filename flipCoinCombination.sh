02b4151d9da5af3d3edc5fa054a4c9faa36af632
anubhav230 <anubhavsingh230@gmail.com>
2020-05-04 13:09:36 +0530

    dublet percentage check

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,24 +2,43 @@
 read -p "Enter how number for flipping the coin" number
 heads=1
 tails=0
-headCount=0
-tailCount=0
+heads2=2
+tails2=3
+hhCount=0
+htCount=0
+thCount=0
+ttCount=0
 count=$number
 declare A dictionary
 while (($count>0))
 do
-	flipCoin=$(( RANDOM%2 ))
+	flipCoin1=$(( RANDOM%2 ))
+	flipCoin2=$(( RANDOM%2 + 2 ))
 
-	if [ $flipCoin -eq 1 ]
+	if [[ $flipCoin1 -eq 1 && $flipCoin2 -eq 2 ]]
 	then
-		dictionary[$count]="heads"
-		((headCount++))
-	else
-		dictionary[$count]="tails"
-		((tailCount++))
+		dictionary[$count]="HH"
+		((hhCount++))
 	fi
+	 if [[ $flipCoin1 -eq 1 && $flipCoin2 -eq 3 ]]
+        then
+                dictionary[$count]="HT"
+                ((htCount++))
+        fi
+	 if [[ $flipCoin1 -eq 0 && $flipCoin2 -eq 3 ]]
+        then
+                dictionary[$count]="TT"
+                ((ttCount++))
+        fi
+	 if [[ $flipCoin1 -eq 0 && $flipCoin2 -eq 2 ]]
+        then
+                dictionary[$count]="TH"
+                ((thCount++))
+        fi
 ((count--))
 done
 echo ${dictionary[@]}
-headPercent=$(echo "scale=2; ($headCount*100)/$number"|bc)
-tailPercent=$(echo "scale=2; ($tailCount*100)/$number"|bc)
+hhPercent=$(echo "scale=2; ($hhCount*100)/$number"|bc)
+htPercent=$(echo "scale=2; ($htCount*100)/$number"|bc)
+ttPercent=$(echo "scale=2; ($ttCount*100)/$number"|bc)
+thPercent=$(echo "scale=2; ($thCount*100)/$number"|bc)
